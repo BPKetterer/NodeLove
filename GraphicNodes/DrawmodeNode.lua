db6b9c5c08360328e682ea1e7b18f78ce3110204
BPKetterer <BenniKe2000@gmail.com>
2025-03-16 01:26:40 +0100

    added abstract class for shapes that have a drawmode setting

---
+++ GraphicNodes/DrawmodeNode.lua
@@ -0,0 +1,40 @@
+local Node = require("NodeLove.CoreNodes.Node")
+
+---@class DrawmodeNode : Node
+---@field protected drawmode string
+---@field protected __index DrawmodeNode
+local DrawmodeNode = setmetatable({}, Node)
+DrawmodeNode.__index = DrawmodeNode
+
+-- ----------------------------- --
+-- PUBLIC DrawmodeNode FUNCTIONS --
+-- ----------------------------- --
+
+---ABSTRACT
+---creates a new node with a Drawmode
+---@param drawmode string|any
+---@return DrawmodeNode: the new point node
+function DrawmodeNode:new(drawmode)
+    local new = setmetatable(Node:new(), DrawmodeNode)
+    ---@cast new DrawmodeNode
+    new:set_drawmode(drawmode)
+    return new
+end
+
+---@generic self : DrawmodeNode
+---@param self self
+---sets the drawmode
+---@param drawmode "fill"|"line"|nil (default: "fill")
+---@return self
+function DrawmodeNode:set_drawmode(drawmode)
+    self.drawmode = drawmode or "fill"
+    return self
+end
+
+---gets the drawmode
+---@return "fill"|"line"
+function DrawmodeNode:get_drawmode()
+    return self.drawmode
+end
+
+return DrawmodeNode
