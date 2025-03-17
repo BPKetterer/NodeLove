e666188f39679f8b933c96202ea71fce4e43d309
BPKetterer <BenniKe2000@gmail.com>
2025-03-17 23:46:16 +0100

    fixed comments

--- GraphicNodes/CircleNode.lua
+++ GraphicNodes/CircleNode.lua
@@ -14,7 +14,7 @@ local CircleNode = CreateClass({
 -- --------------------------- --
 
 ---creates a new point node (see https://love2d.org/wiki/love.graphics.circle)
----@return CircleNode: the new arc node
+---@return CircleNode: the new circle node
 function CircleNode:new()
     local new = setmetatable(Node:new(), CircleNode)
     ---@cast new CircleNode
@@ -37,12 +37,12 @@ function CircleNode:get_radius()
     return self.radius
 end
 
--- --------------------------- --
--- PROTECTED ArcNode FUNCTIONS --
--- --------------------------- --
+-- ------------------------------ --
+-- PROTECTED CircleNode FUNCTIONS --
+-- ------------------------------ --
 
 ---@protected
----draws the arc
+---draws the circle
 function CircleNode:draw_bottom()
     love.graphics.circle(self.drawmode, 0, 0, self.radius, self.segments)
 end
