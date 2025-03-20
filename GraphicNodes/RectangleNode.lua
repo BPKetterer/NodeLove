80154d59c551cd54ec5720bffd4783fa6f13d3f4
BPKetterer <BenniKe2000@gmail.com>
2025-03-21 00:57:09 +0100

    removed resizable extension

--- GraphicNodes/RectangleNode.lua
+++ GraphicNodes/RectangleNode.lua
@@ -2,15 +2,18 @@ require("NodeLove.Utils.ClassManager")
 local Node = require("NodeLove.CoreNodes.Node")
 local SegmentExtension = require("NodeLove.GraphicNodes.Extensions.SegmentExtension")
 local DrawmodeExtension = require("NodeLove.GraphicNodes.Extensions.DrawmodeExtension")
-local ResizableExtension = require("NodeLove.GraphicNodes.Extensions.ResizableExtension")
 
----@class RectangleNode : Node, SegmentExtension, DrawmodeExtension, ResizableExtension
+---@class RectangleNode : Node, SegmentExtension, DrawmodeExtension
 ---@field protected rx number
 ---@field protected ry number
+---@field protected width number
+---@field protected height number
 local RectangleNode = CreateClass({
     rx = 0,
-    ry = 0
-}, Node, SegmentExtension, DrawmodeExtension, ResizableExtension)
+    ry = 0,
+    width = 0,
+    height = 0
+}, Node, SegmentExtension, DrawmodeExtension)
 
 -- ------------------------------ --
 -- PUBLIC RectangleNode FUNCTIONS --
@@ -22,6 +25,38 @@ function RectangleNode:new()
     local new = setmetatable(Node:new(), RectangleNode)
     ---@cast new RectangleNode
     return new
+end
+
+---@generic self : RectangleNode
+---@param self self
+---sets the width of the rectangle
+---@param width number
+---@return self
+function RectangleNode:set_width(width)
+    self.width = width
+    return self
+end
+
+---gets the width of the rectangle
+---@return number
+function RectangleNode:get_width()
+    return self.width
+end
+
+---@generic self : RectangleNode
+---@param self self
+---sets the height of the rectangle
+---@param height number
+---@return self
+function RectangleNode:set_height(height)
+    self.height = height
+    return self
+end
+
+---gets the height of the rectangle
+---@return number
+function RectangleNode:get_height()
+    return self.height
 end
 
 ---@generic self : RectangleNode
