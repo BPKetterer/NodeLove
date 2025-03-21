27db2ba22b152e5b2d2cdeacc7b0f4451ce3899e
BPKetterer <BenniKe2000@gmail.com>
2025-03-21 01:01:01 +0100

    modifieded include path in init

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 ---@module "NodeLove"
 local NodeLove = {}
 
-NodeLove.Utils = require("NodeLove.Utils")
-NodeLove.CoreNodes = require("NodeLove.CoreNodes")
-NodeLove.GraphicNodes = require("NodeLove.GraphicNodes")
+NodeLove.Utils = require("NodeLove.Utils.init")
+NodeLove.CoreNodes = require("NodeLove.CoreNodes.init")
+NodeLove.GraphicNodes = require("NodeLove.GraphicNodes.init")
 
 return NodeLove
