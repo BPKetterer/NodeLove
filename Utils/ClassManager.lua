a2b66783f7a5f3c3f645ea1e723f085308c4dd87
BPKetterer <BenniKe2000@gmail.com>
2025-03-22 04:38:42 +0100

    updated class manager priorities in inheritance

--- Utils/ClassManager.lua
+++ Utils/ClassManager.lua
@@ -1,13 +1,15 @@
 ---creates a class
 ---the parents variables and funcitons are copied in a shallow copy
----the parents are copied from left to right
+---in case of conflicts the leftmost carries
 ---@param variables table
 ---@param ... table the parents
 ---@return table
 function CreateClass(variables, ...)
     for _, parent in ipairs({...}) do
         for k, v in pairs(parent) do
-            variables[k] = v
+            if not variables[k] then
+                variables[k] = v
+            end
         end
     end
     variables.__index = variables
